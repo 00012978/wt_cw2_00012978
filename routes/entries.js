97499eaafae39bd5b2acc75b1fe9b46f79828452
00012978 <113568714+00012978@users.noreply.github.com>
2023-04-02 10:44:58 +0500

    add update and delete functionality

--- routes/entries.js
+++ routes/entries.js
@@ -2,20 +2,24 @@ const express = require('express');
 const router = express.Router();
 const entriesController = require('./entriesController');
 const axios = require('axios');
+const { check, validationResult } = require('express-validator');
 const validate = require('./validate');
 
+// get all entries
 router.route('/')
     .get(entriesController.getAllEntries);
 
+// create route
 router.route('/create')
     .get((req, res)=>{
         res.render('create');
     })
     .post(validate, entriesController.createEntry);
 
-
+// get singly entry route
 router.get('/id/:id', entriesController.getEntryById);
 
+// view signle entry route 
 router.get('/view/:id', async (req, res) => {       
     const apiUrl = `${process.env.API_BASE_URL}/api/entries/id/${req.params.id}`;
     try {
@@ -31,5 +35,41 @@ router.get('/view/:id', async (req, res) => {
         }
     }
 });
+
+// update route
+router.route('/update/:id')
+    .get(async (req, res) => {
+        const apiUrl = `${process.env.API_BASE_URL}/api/entries/id/${req.params.id}`;
+        try {
+            const response = await axios.get(apiUrl);
+            const entry = response.data;
+            console.log(`entries variable at get(/update/:id) = ${entry}`)
+            res.render('edit', {entry: entry, messages: ''});
+        } catch (error) {
+            if (error.response && error.response.status === 404) {
+            res.status(404).render('error', { msg: 'Entry not found' });
+            } else {
+            res.render('error', { msg: 'Something went wrong' });
+            }
+        }
+    })
+    .post(validate, entriesController.updateEntry);
+
+// delete route
+router.route('/delete/:id')
+    .get(async (req, res) => {
+        const apiUrl = `${process.env.API_BASE_URL}/api/entries/delete/${req.params.id}`;
+        try {
+        const response = await axios.delete(apiUrl);
+        res.redirect('/'); }
+        catch (err){
+            if (err.response && err.response.status === 404) {
+                res.status(404).render('error', { msg: 'Entry not found' });
+                } else {
+                res.status(500).render('error', { msg: 'Something went wrong' });
+                }        
+            }
+        })
+    .delete(entriesController.deleteEntry);
 
 module.exports = router;
--- routes/entriesController.js
+++ routes/entriesController.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path')
 
 const { v4: uuidv4 } = require('uuid');
-const { validationResult } = require('express-validator');
+const { check, validationResult } = require('express-validator');
 
 const dbPath = path.join(__dirname, '..', 'models', 'entriesDb.json');
 
@@ -18,8 +18,46 @@ const getAllEntries = (req, res) => {
             }
         })
     } catch (err) {
-        res.status(500).json(`Error ${err}`);
+        res.status(500).render('error', {msg:`Error ${err}`});
     }
+}
+
+const createEntry = (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()){
+        return res.render('create', {messages: errors.array()}); 
+    }
+    try {
+        let newEntry = {
+            id: uuidv4(),
+            title: req.body.title,
+            temperature: req.body.temperature,
+            bloodPressure: [req.body.sysPressure, req.body.diaPressure],
+            saturation: req.body.saturation, 
+            date: req.body.date,
+            addInfo: req.body.addInfo,
+            edited: new Date().toISOString()
+        }
+
+        fs.readFile(dbPath, 'utf8', (err, data) =>{
+            if (err) throw err;
+    
+            let entries = [];
+            if (data) {
+                entries = JSON.parse(data);
+            }
+            entries = entries.concat(newEntry);
+    
+            fs.writeFile(dbPath, JSON.stringify(entries), err => {
+                if (err) throw err;
+                return res.render('create', {messages: {msg:'Entry was added successfully!'}});  
+            });   
+         })
+
+    } catch (error) {
+        res.status(500).render('error', {msg:`Error ${err}`});
+    }
+
 }
 
 const getEntryById = (req, res) => {
@@ -44,48 +82,90 @@ const getEntryById = (req, res) => {
     }
 }
 
-const createEntry = (req, res) => {
+const updateEntry = (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()){
-        console.log(`There were errors(log from createEntry): ${errors.array()}, ${JSON.stringify(errors.array())}`)
-        return res.render('create', {messages: errors.array()}); 
+        console.log(`req body in update is equal to ${JSON.stringify(req.body)}`);
+        return res.render('edit', {messages: errors.array(), entry: req.body}); 
     }
     try {
-        let newEntry = {
-            id: uuidv4(),
-            title: req.body.title,
-            temperature: req.body.temperature,
-            bloodPressure: [req.body.sysPressure, req.body.diaPressure],
-            saturation: req.body.saturation, 
-            date: req.body.date,
-            edited: new Date().toISOString()
+      const { title, temperature, sysPressure, diaPressure, saturation, date, addInfo } = req.body;
+      const bloodPressure = [sysPressure, diaPressure];
+      const entryId = req.params.id;
+  
+      fs.readFile(dbPath, 'utf8', (err, data) => {
+        if (err) throw err;
+  
+        const entries = JSON.parse(data);
+        const entryIndex = entries.findIndex(entry => entry.id === entryId);
+  
+        if (entryIndex === -1) {
+          return res.status(404).render('error', {
+            msg: `Entry with id ${entryId} not found`,
+          });
         }
+  
+        entries[entryIndex] = {
+          ...entries[entryIndex],
+          title,
+          temperature,
+          bloodPressure,
+          saturation,
+          date,
+          addInfo,
+          edited: new Date().toISOString()
+        };
+  
+        fs.writeFile(dbPath, JSON.stringify(entries), err => {
+          if (err) throw err;
+          return res.render('viewEntry', {entry: entries[entryIndex], messages: {msg:'Entry was updated successfully!'}});
+        });
+      });
+    } catch (error) {
+      res.status(500).render('error', {msg: `Error ${err}`});
+    }
+  };
+  
+  
 
+
+const deleteEntry = (req, res) => {
+    try {
         fs.readFile(dbPath, 'utf8', (err, data) =>{
             if (err) throw err;
-    
-            let entries = [];
-            if (data) {
-                entries = JSON.parse(data);
-            }
-            entries = entries.concat(newEntry);
-    
-            fs.writeFile(dbPath, JSON.stringify(entries), err => {
-                if (err) throw err;
-                console.log(`I'm creating file (line 54). here is the data ${JSON.stringify(entries)}`)
-                return res.render('create', {messages: {msg:'Entry was added successfully!'}});  
-            });   
-         })
 
+            let entry;
+
+            let allEntries = [];
+            if (data.length > 0){
+                allEntries = JSON.parse(data);
+                entry = allEntries.filter(entry => entry.id === req.params.id)[0];
+            }
+
+            if (!entry){
+                return res.status(404).json({
+                    msg: `Entry ${req.params.id} not found`
+                });
+            }
+        
+            const filteredArray = allEntries.filter(entry => entry.id !== req.params.id);
+            data = filteredArray;
+        
+            fs.writeFile(dbPath, JSON.stringify(data), (err) => {
+                if (err) throw err;
+                res.status(200).json(data);
+              })
+         })
     } catch (error) {
         res.status(500).send(`Error ${err}`);
     }
-
 }
 
 
 module.exports = {
     getAllEntries,
     createEntry,
-    getEntryById
+    getEntryById,
+    updateEntry,
+    deleteEntry,
 }
